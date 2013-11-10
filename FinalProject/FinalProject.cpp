c134d9975cbf20e50f070310c799dc010a97362c
Adam Phung <adam.phung@gmail.com>
2013-11-10 18:47:51 -0500

    Did animation demo as per tutorial

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -6,15 +6,49 @@ const int WINDOW_HEIGHT = 480;
 const int WIN_LOC_X = 600;
 const int WIN_LOC_Y = 300;
 
-void renderScene(void) {
+float angle = 0.0f;
 
+void changeSize(int width, int height) {
+	if (height == 0)
+		height = 1;
+	float ratio = width * 1.0 / height;
+
+	// Use Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+
+	// Reset Matrix
+	glLoadIdentity();
+
+	// Set viewport to the entire window
+	glViewport(0, 0, width, height);
+
+	// Set correct perspective
+	gluPerspective(45, ratio, 1, 100);
+
+	// Get back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	// Reset Transformations
+	glLoadIdentity();
+
+	// Set camera
+	gluLookAt(	0.0f, 0.0f, 10.0f,
+				0.0f, 0.0f, 0.0f,
+				0.0f, 1.0f, 0.0f);
+
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+
 	glBegin(GL_TRIANGLES);
-		glVertex3f(-0.5,-0.5,0.0);
-		glVertex3f(0.5,0.0,0.0);
-		glVertex3f(0.0,0.5,0.0);
+		glVertex3f(-2,-2,-5.0);
+		glVertex3f(2,0.0,-5.0);
+		glVertex3f(0.0,2,-5.0);
 	glEnd();
+
+	angle += 0.1f;
 
     glutSwapBuffers();
 }
@@ -29,6 +63,10 @@ int main(int argc, char **argv) {
 
 	// register callbacks
 	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+
+	// Idle Function Registration
+	glutIdleFunc(renderScene);
 
 	// enter GLUT event processing cycle
 	glutMainLoop();

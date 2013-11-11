7e22fd06d21862fdc6ac713f94035ac3dfcb0fc4
Adam Phung <adam.phung@gmail.com>
2013-11-11 10:57:53 -0500

    keyboard snowman tutorial

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <glut.h>
+#include <math.h>
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
@@ -7,7 +8,35 @@ const int WIN_LOC_X = 600;
 const int WIN_LOC_Y = 300;
 
 float angle = 0.0f;
+float lx = 0.0f, lz = -1.0f;
+float x = 0.0f, z = 5.0f;
 float red = 1.0f, green = 1.0f, blue = 1.0f;
+
+void drawSnowMan() {
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	// Draw Body
+	glTranslatef(0.0f ,0.75f, 0.0f);
+	glutSolidSphere(0.75f,20,20);
+
+	// Draw Head
+	glTranslatef(0.0f, 1.0f, 0.0f);
+	glutSolidSphere(0.25f,20,20);
+
+	// Draw Eyes
+	glPushMatrix();
+	glColor3f(0.0f,0.0f,0.0f);
+	glTranslatef(0.05f, 0.10f, 0.18f);
+	glutSolidSphere(0.05f,10,10);
+	glTranslatef(-0.1f, 0.0f, 0.0f);
+	glutSolidSphere(0.05f,10,10);
+	glPopMatrix();
+
+	// Draw Nose
+	glColor3f(1.0f, 0.5f , 0.5f);
+	glRotatef(0.0f,1.0f, 0.0f, 0.0f);
+	glutSolidCone(0.08f,0.5f,10,2);
+}
 
 void changeSize(int width, int height) {
 	if (height == 0)
@@ -31,28 +60,38 @@ void changeSize(int width, int height) {
 }
 
 void renderScene(void) {
+
+	// Clear Color and Depth Buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Reset Transformations
+	// Reset transformations
 	glLoadIdentity();
 
-	// Set camera
-	gluLookAt(	0.0f, 0.0f, 10.0f,
-				0.0f, 0.0f, 0.0f,
-				0.0f, 1.0f, 0.0f);
+	// Set the camera
+	gluLookAt(	x, 1.0f, z,
+			x+lx, 1.0f,  z+lz,
+			0.0f, 1.0f,  0.0f);
 
-	glRotatef(angle, 0.0f, 1.0f, 0.0f);
-
-	glColor3f(red, green, blue);
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-2.0, -2.0, 0.0);
-		glVertex3f(2.0, 0.0, 0.0);
-		glVertex3f(0.0, 2.0, 0.0);
+    // Draw ground
+	glColor3f(0.9f, 0.9f, 0.9f);
+	glBegin(GL_QUADS);
+		glVertex3f(-100.0f, 0.0f, -100.0f);
+		glVertex3f(-100.0f, 0.0f,  100.0f);
+		glVertex3f( 100.0f, 0.0f,  100.0f);
+		glVertex3f( 100.0f, 0.0f, -100.0f);
 	glEnd();
 
-	angle += 0.1f;
+    // Draw 36 SnowMen
+	for(int i = -3; i < 3; i++) {
+		for(int j=-3; j < 3; j++) {
+			glPushMatrix();
+			glTranslatef(i*10.0,0,j * 10.0);
+			drawSnowMan();
+			glPopMatrix();
+		}
+	}
 
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
 
 void processNormalKeys(unsigned char key, int x, int y) {
@@ -69,22 +108,27 @@ void processNormalKeys(unsigned char key, int x, int y) {
 }
 
 void processSpecialKeys(int key, int x, int y) {
-	switch(key) {
-	case GLUT_KEY_F1:
-		red = 1.0;
-		green = 0.0;
-		blue = 0.0;
-		break;
-	case GLUT_KEY_F2:
-		red = 0.0;
-		green = 1.0;
-		blue = 0.0;
-		break;
-	case GLUT_KEY_F3:
-		red = 0.0;
-		green = 0.0;
-		blue = 1.0;
-		break;
+	float fraction = 0.5f;
+
+	switch (key) {
+		case GLUT_KEY_LEFT :
+			angle -= 0.01f;
+			lx = sin(angle);
+			lz = -cos(angle);
+			break;
+		case GLUT_KEY_RIGHT :
+			angle += 0.01f;
+			lx = sin(angle);
+			lz = -cos(angle);
+			break;
+		case GLUT_KEY_UP :
+			x += lx * fraction;
+			z += lz * fraction;
+			break;
+		case GLUT_KEY_DOWN :
+			x -= lx * fraction;
+			z -= lz * fraction;
+			break;
 	}
 }
 
@@ -104,6 +148,9 @@ int main(int argc, char **argv) {
 	// Keyboard Calls
 	glutKeyboardFunc(processNormalKeys);
 	glutSpecialFunc(processSpecialKeys);
+
+	// OpenGL init
+	glEnable(GL_DEPTH_TEST);
 
 	// enter GLUT event processing cycle
 	glutMainLoop();
